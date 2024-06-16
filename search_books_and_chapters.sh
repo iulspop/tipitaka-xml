2c342f8025174d8a595399add285cec11ff7fc2d
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2024-06-16 13:07:12 -0400

    add search_books script

---
+++ search_books_and_chapters.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for file in ./romn/e*; do
+    match_book=$(rg -o '<p rend="book">.*?</p>' "$file")
+    if [[ -n $match_book ]]; then
+        clean_match=$(echo "$match_book" | sed -E 's/<p rend="book">//; s:</p>::; s:<pb[^>]*>::g')
+        echo "$file:$clean_match"
+    else
+        match_chapter=$(rg -o '<p rend="chapter">.*?</p>' "$file" | head -n 1)
+        if [[ -n $match_chapter ]]; then
+            clean_match=$(echo "$match_chapter" | sed -E 's/<p rend="chapter">//; s:</p>::; s:<pb[^>]*>::g')
+            echo "$file:$clean_match"
+        fi
+    fi
+done
